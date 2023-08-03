e74b14d8d7ba187503833a2acba96627e840a066
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-08-03 14:43:14 -0300

    aula
    declarcaoVariavel and primeiroCodigo

---
+++ modulo_2/declaracaoVariavel.js
@@ -0,0 +1,8 @@
+let variavelNova = 'valorDessaVariavel';
+variavelNova = 'novoValor';
+
+const ultimoValorDaAula = 'Finalmante coloquei o valor na variável';
+
+console.log(variavelNova);
+console.log(ultimoValorDaAula);
+
---
+++ modulo_2/primeiroCodigo.js
@@ -0,0 +1,14 @@
+let mensagemTela = 'Olá! Essa é a minha primeira variável em Js!';
+
+
+console.log(mensagemTela)
+console.log(mensagemTela)
+console.log(mensagemTela)
+console.log(mensagemTela)
+console.log(mensagemTela)
+
+
+
+mensagemTela = 'Mudança de mensagem';
+
+console.log(mensagemTela)
