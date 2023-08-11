ea5d89e00fceaf193d8d0a6e468f9aaffbd1ea8a
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-08-11 17:11:50 -0300

    Update declaracaoVariavel.js

--- modulo_2/declaracaoVariavel.js
+++ modulo_2/declaracaoVariavel.js
@@ -6,3 +6,10 @@ const ultimoValorDaAula = 'Finalmante coloquei o valor na variável';
 console.log(variavelNova);
 console.log(ultimoValorDaAula);
 
+let variNova = 'valorDessaVariavel';
+variNova = 'novoValor';
+
+const ultValor = 'Finalmante coloquei o valor na variável';
+
+console.log(variNova);
+console.log(ultValor);
